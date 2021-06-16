fb9246326f98e43c35da86b7ecf5dd6a85229147
Nitesh Pidiparas <niteshpidiparas76@gmail.com>
2021-06-16 13:44:50 +0530

    initial commit

---
+++ store/views/home.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from store.models.product import Product
+from store.models.category import Category
+from store.models.contact import Contact
+from django.contrib import messages
+from django.views import View
+
+
+# Create your views here.
+class Index(View):
+
+    def post(self, request):
+        product = request.POST.get('product')
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity <= 1:
+                        cart.pop(product)
+                    else:
+                        cart[product] = quantity-1
+                else:
+                    cart[product] = quantity+1
+
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+
+        request.session['cart'] = cart
+        print('cart', request.session['cart'])
+        return redirect('homepage')
+
+    def get(self, request):
+        # print()
+        return HttpResponseRedirect(f'/store{request.get_full_path()[1:]}')
+
+
+def store(request):
+    cart = request.session.get('cart')
+    if not cart:
+        request.session['cart'] = {}
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products()
+
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+
+    print('you are : ', request.session.get('email'))
+    return render(request, 'index.html', data)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 4:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            contact = Contact(name=name, email=email,
+                              phone=phone, content=content)
+            contact.save()
+            messages.success(
+                request, "Your message has been successfully sent")
+    return render(request, "contact.html")
+
+
+def search(request):
+    query = request.GET['query']
+    if len(query) > 78:
+        allProducts = Product.objects.none()
+    else:
+        allProductsName = Product.objects.filter(name__icontains=query)
+        allProductsDescription = Product.objects.filter(
+            description__icontains=query)
+        allProducts = allProductsName.union(allProductsDescription)
+    if allProducts.count() == 0:
+        messages.warning(
+            request, "No search results found. Please refine your query.")
+    params = {'allProducts': allProducts, 'query': query}
+    return render(request, 'search.html', params)
